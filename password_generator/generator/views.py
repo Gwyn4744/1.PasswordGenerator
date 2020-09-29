5131295d2d8a9749a5dc0a85c7e8b76f373f3a58
Dominik Bednarski <dominikbednarski@wp.pl>
2020-09-29 13:38:01 +0200

    Modificated password view - show random password from constant values - 0.0.5

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -7,4 +8,12 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('qwertyuiopasdfghjklzxcvbnm')
+    length = 10
+
+    my_password = ''
+    for x in range(length):
+        my_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':my_password})
