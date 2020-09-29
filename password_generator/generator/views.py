2e0de0d11f5379e70e0e3b4d66d2b0db29d3e0d0
Dominik Bednarski <dominikbednarski@wp.pl>
2020-09-29 13:09:18 +0200

    Added the page for show generated password - 0.0.4

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'generator/home.html')
+
+def password(request):
+    return render(request, 'generator/password.html')
